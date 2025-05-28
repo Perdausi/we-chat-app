a18732a22c7f4eb1d4aa294999bff36807a20b75
Perdausi Hairul <perdausi11@gmail.com>
2025-05-28 22:46:23 +0800

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -14,10 +15,19 @@ app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// this line will convert the file into a static file
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+// then this line will allow us to run our frontend in the server as well
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port,()=> {
     connectDB();
